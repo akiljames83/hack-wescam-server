195d5bff9fa4a154b456904654d62b62964db717
Igor Grishchenko <devgrishchenko@gmail.com>
2018-05-04 22:10:27 -0400

    Initial Commit

---
+++ hack-wescam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hack-wescam
+//
+//  Created by Igor Grishchenko on 2018-05-04.
+//  Copyright © 2018 Igor Grishchenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
