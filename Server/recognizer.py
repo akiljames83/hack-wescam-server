2bbdf843cb3b88c029836030933af8aee0c4ca54
Igor Grishchenko <devgrishchenko@gmail.com>
2018-05-08 16:50:47 -0400

    Clean up

--- Server/recognizer.py
+++ Server/recognizer.py
@@ -11,7 +11,6 @@ try:
 
     img = io.imread('imgs/detect.jpg')
     dets = detector(img, 1)
-
     for k, d in enumerate(dets):
         shape = sp(img, d)
 
@@ -23,7 +22,7 @@ try:
 
     for k, d in enumerate(dets_webcam):
         shape = sp(img2, d)
-        
+
     face_descriptor2 = facerec.compute_face_descriptor(img2, shape)
 
     print(distance.euclidean(face_descriptor1, face_descriptor2))
